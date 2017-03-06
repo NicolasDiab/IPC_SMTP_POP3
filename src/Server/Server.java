5cba963032a44483f59159a7c34bb0a210d11db9
NicolasDiab <nicolas-diab@hotmail.fr>
2017-03-06 08:33:17 +0100

    constructeur

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -4,4 +4,21 @@ package Server;
  * Created by Nicolas on 06/03/2017.
  */
 public class Server {
+    public int ip;
+    public int port;
+
+    public Server (int ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    /*
+    try{
+ServerSocket myconnex = new ServerSocket(80,6) ;
+}
+catch(IOException ex){
+system.err.println(ex);
+}
+
+     */
 }
