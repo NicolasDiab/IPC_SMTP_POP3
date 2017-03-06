47452a31a12469010851e1e71f28bb2be1837e0b
GregoirePiat <gregoire.piat@outlook.fr>
2017-03-06 09:24:00 +0100

    Fixed issue on Server class

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,5 +1,6 @@
 package Server;
 
+import javax.sound.midi.SysexMessage;
 import java.io.*;
 import java.net.*;
 import java.util.Arrays;
@@ -13,23 +14,18 @@ public class Server {
     /**
      * State constants
      */
-    private const String STATE_LISTENING = "LISTENING";
-    private const String STATE_AUTHORIZATION = "AUTHORIZATION";
-    private const String STATE_TRANSACTION = "TRANSACTION";
-    private const String STATE_UPDATE = "UPDATE";
-    private const String STATE_CLOSED = "CLOSED";
+    private String STATE_LISTENING = "LISTENING";
+    private String STATE_AUTHORIZATION = "AUTHORIZATION";
+    private String STATE_TRANSACTION = "TRANSACTION";
+    private String STATE_UPDATE = "UPDATE";
+    private String STATE_CLOSED = "CLOSED";
 
-    private const String MESSAGE_HELLO = "+OK POP3 server ready";
+    private String MESSAGE_HELLO = "+OK POP3 server ready";
 
-
-    private int ip;
     private int port;
     private String state;
 
-
-
-    public Server (int ip, int port) {
-        this.ip = ip;
+    public Server(int port) {
         this.port = port;
         this.state = STATE_CLOSED;
     }
@@ -44,20 +40,23 @@ public class Server {
             this.state = STATE_LISTENING;
             int currentByte = -1;
             while (true){
-                Socket connexion = server.accept();
+                Socket connexion = myconnex.accept();
                 try {
                     InputStream is = connexion.getInputStream();
                     BufferedInputStream bis = new BufferedInputStream(is);
-                    bis.write(MESSAGE_HELLO.getBytes());
+                    //bis.write(MESSAGE_HELLO.getBytes());
 
 
+                }
+                catch(IOException ex){
+                    System.err.println(ex);
                 }
             }
         }
         catch(IOException ex){
-            system.err.println(ex);
+            System.err.println(ex);
         }
-
+        return 0;
     }
 
 }
