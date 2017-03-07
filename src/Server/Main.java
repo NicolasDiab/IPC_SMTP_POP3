a9769e1e50e89f23845c13260f12b01c186c503c
NicolasDiab <nicolas-diab@hotmail.fr>
2017-03-07 08:25:07 +0100

    prepare for mail class

--- src/Server/Main.java
+++ src/Server/Main.java
@@ -3,6 +3,7 @@ package Server;
 public class Main {
     public static void main(String [ ] args)
     {
+        // on a mis 2001 et non le port habituel pour des problèmes de disponibilité de port
         Server server = new Server(2001);
 
         server.launch();
